14daee9655ae5335add99d195b6c80be003a3953
That1Panda <65992395+That1Panda@users.noreply.github.com>
2022-05-25 09:07:33 +0200

    Final

--- ButtonB.c
+++ ButtonB.c
@@ -38,8 +38,53 @@ loop:		clear_display();
 					
 					
 					
+					if(time%2==0){
+						minutes=time/2; //taking the integer part and turning it into char ex: digit_to_char(4.5)=4
+		
+						seconds=0; //returning 30/0 seconds ex: (int)(30*(5-4))= 30
+						timer_display(minutes,seconds);
+						if(start(minutes,seconds) ==1){
+						char k ;
+						for (k=0;k<3;k++){
+							RGB_output(0);
+							SysTick_wait_10ms(1*100);
+							RGB_output(14);
+							SysTick_wait_10ms(1*100);
+							}
+						RGB_output(0);
+						clear_display();
+						return;}
+			
 					}
-				
+					else{
+						minutes=(time-1)/2;//taking the integer part and turning it into char ex: digit_to_char(4.5)=4
+		
+						seconds=30;
+						timer_display(minutes,seconds);
+						if(start(minutes,seconds) ==1){
+							buzzer_RGB_blinking();
+						
+						
+							RGB_output(0);
+							clear_display();
+							return;}
+					}
+					
+					
+		
+					
+		
+				}
+
+			
+				else {
+					
+					clear_display();
+					LCD_string_display("ERR"); // show the error message and keep it for two seconds then restart the loop with clearing  display
+					SysTick_wait_10ms(2*100);
+					goto loop;
+				}
+					
 		
 
 
---
+++ buttonB - Copy.h
@@ -0,0 +1,7 @@
+#include "lcd.h"
+#include "keypad.h"
+#include "switches.h"
+#include "timer.h"
+#include "buzzer.h"
+
+void B();
